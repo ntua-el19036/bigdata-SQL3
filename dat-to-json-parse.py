f52f681e85c12fa37cb33a862cc7691400ba6342
Petros Panigyrakis <115503644+ntua-el19036@users.noreply.github.com>
2023-12-17 14:49:22 +0200

    Added tables

--- dat-to-json-parse.py
+++ dat-to-json-parse.py
@@ -27,7 +27,35 @@ tables_dict = [
         'table_name': 'ship_mode',
         'file_path': os.path.join(script_directory, 'data', 'ship_mode.dat'),
         'column_names': ['sm_ship_mode_sk','sm_ship_mode_id','sm_type','sm_code','sm_carrier','sm_contract']
-    }
+    },
+    {
+        'table_name': 'store',
+        'file_path': os.path.join(script_directory, 'data', 'store.dat'),
+        'column_names': ['s_store_sk','s_store_id','s_rec_start_date','s_rec_end_date','s_closed_date_sk','s_store_name',
+                         's_number_employees','s_floor_space','s_hours','S_manager','S_market_id','S_geography_class',
+                         'S_market_desc','s_market_manager','s_division_id','s_division_name','s_company_id','s_company_name',
+                         's_street_number','s_street_name','s_street_type','s_suite_number','s_city','s_county',
+                         's_state','s_zip','sm_type','s_country','s_gmt_offset','s_tax_percentage'
+                        ]
+    },
+    {
+        'table_name': 'store_returns',
+        'file_path': os.path.join(script_directory, 'data', 'store_returns.dat'),
+        'column_names': ['sr_returned_date_sk','sr_return_time_sk','sr_item_sk','sr_customer_sk','sr_cdemo_sk','sr_hdemo_sk',
+                         'sr_addr_sk','sr_store_sk','sr_reason_sk','sr_ticket_number','sr_return_quantity','sr_return_amt',
+                         'sr_return_tax','sr_return_amt_inc_tax','sr_fee','sr_return_ship_cost','sr_refunded_cash','sr_reversed_charge',
+                         'sr_store_credit','sr_net_loss'
+                        ]
+    },
+    {
+        'table_name': 'store_sales',
+        'file_path': os.path.join(script_directory, 'data', 'store_sales.dat'),
+        'column_names': ['ss_sold_date_sk','ss_sold_time_sk','ss_item_sk','ss_customer_sk','ss_cdemo_sk','ss_hdemo_sk',
+                         'ss_addr_sk','ss_store_sk','ss_promo_sk','ss_ticket_number','ss_quantity','ss_wholesale_cost',
+                         'ss_list_price','ss_sales_price','ss_ext_discount_amt','ss_ext_sales_price','ss_ext_wholesale_cost','ss_ext_list_price',
+                         'ss_ext_tax','ss_coupon_amt','ss_net_paid','ss_net_paid_inc_tax','ss_net_profit'
+                        ]
+    },
 ]
 
 for table in tables_dict:

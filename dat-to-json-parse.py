ebdb4e7c399b8b877b14602ef65467ecbb3fad2c
Petros Panigyrakis <el19036@mail.ntua.gr>
2024-01-03 20:07:50 +0200

    parse .dat files in batches, now deletes .dat files unless --keep-source is used

--- dat-to-json-parse.py
+++ dat-to-json-parse.py
@@ -1,6 +1,7 @@
 import json
 import os
-from config.tables import tables_dict
+import argparse
+from config.tables_redis_for_json import tables_dict
 
 def parse_and_transform(line, column_names):
     columns = line.strip().split('|')
@@ -12,29 +13,57 @@ def transform_sql_to_key_value(sql_data):
 
 def write_to_file(data, filename, mode='a'):
     with open(filename, mode) as file:
-        json.dump(data, file)
-        file.write('\n')
+        for line in data:
+            json.dump(line, file)
+            file.write('\n')
 
-# Get the directory of the script
-script_directory = os.path.dirname(os.path.abspath(__file__))
-parent_directory = os.path.dirname(script_directory)
+def process_file_in_batches(file_path, output_filename, column_names, batch_size=100):
+    with open(file_path, 'r') as file:
+        batch = []
+        for line in file:
+            sql_data = parse_and_transform(line, column_names)
+            key_value_data = transform_sql_to_key_value(sql_data)
+            batch.append(key_value_data)
 
-# Create a folder named 'json_data' in the script directory
-output_folder = os.path.join(parent_directory, 'json_data')
-os.makedirs(output_folder, exist_ok=True)
+            # Write to the file in batches
+            if len(batch) == batch_size:
+                write_to_file(batch, output_filename, mode='a')
+                batch = []
 
-# Read .dat file and write key-value data to a single file
-for table in tables_dict:
+        # Write the remaining data in the last batch
+        if batch:
+            write_to_file(batch, output_filename, mode='a')
+
+def main():
+    parser = argparse.ArgumentParser(description='Process .dat files and create JSON files.')
+    parser.add_argument('--keep-source', action='store_true', help='Keep the source .dat file after processing')
+
+    args = parser.parse_args()
+
+    # Get the directory of the script
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    parent_directory = os.path.dirname(script_directory)
+
+    # Create a folder named 'json_data' in the script directory
+    output_folder = os.path.join(parent_directory, 'json_data')
+    os.makedirs(output_folder, exist_ok=True)
+
+    # for table in tables_dict:
+    table = tables_dict[14]
     file_path = table['file_path']
-    output_filename = os.path.join(output_folder, table['table_name'] + '.json')
+    output_filename = os.path.join(output_folder, table['table_name'] + 'test.json')
     column_names = table['column_names']
 
     # Check if the output file exists, and if it does, clear its content
     if os.path.exists(output_filename):
         open(output_filename, 'w').close()
 
-    with open(file_path, 'r') as file:
-        for line in file:
-            sql_data = parse_and_transform(line, column_names)
-            key_value_data = transform_sql_to_key_value(sql_data)
-            write_to_file(key_value_data, output_filename)
+    # Process the file in batches and write to the output file
+    process_file_in_batches(file_path, output_filename, column_names)
+
+    # Remove the initial file only if the --keep-source argument is not provided
+    if not args.keep_source:
+        os.remove(file_path)
+
+if __name__ == "__main__":
+    main()

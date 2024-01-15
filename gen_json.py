5eddb61882d3dc50f9c6b43a621cd97ed50ecf2f
Petros Panigyrakis <el19036@mail.ntua.gr>
2024-01-15 18:35:45 +0200

    split into files, also updated data and output directories

--- gen_json.py
+++ gen_json.py
@@ -5,6 +5,15 @@ import subprocess
 from tqdm import tqdm
 from config.tables_redis_for_json import tables_dict
 
+# Create a folder named 'json_data' in the script directory
+#output_folder = os.path.join(parent_directory, 'json_data')
+output_folder = '/media/user/fs2/json_data'
+os.makedirs(output_folder, exist_ok=True)
+
+#data_folder = os.path.join(parent_directory, 'data')
+data_folder = '/media/user/fs2/data'
+os.makedirs(data_folder, exist_ok=True)
+
 def parse_and_transform(line, column_names):
     columns = line.strip().split('|')
     values_dict = {column_names[i]: columns[i] for i in range(0, len(columns)-1)}
@@ -23,19 +32,28 @@ def get_total_lines(file_path):
 
 def process_file_in_batches(file_path, output_folder, column_names, table_name, batch_size=1000000):
     sed_command = f"sed -i '1,{batch_size}d' {file_path}"
-    counter = 1
-    total_lines = get_total_lines(file_path)
+    counter = 0
+    #total_lines = get_total_lines(file_path)
     # Use tqdm for progress bar
-    with tqdm(total=total_lines, desc=f"Processing {table_name}", unit="line") as pbar:
+    #with tqdm(total=total_lines, desc=f"Processing {table_name}", unit="line") as pbar:
+    flag = False
+    for counter in tqdm(range(10000)):
         flag = False
-        while True:
-            flag = False
-            batch = []
-            with open(file_path, 'r') as file:
+        batch = []
+        file_idx = f"{counter}"
+        if(counter<1000):
+            file_idx = "0"+file_idx
+        if(counter<100):
+            file_idx = "0"+file_idx
+        if(counter<10):
+            file_idx = "0"+file_idx
+        segment_file=file_path+file_idx
+        if (os.path.exists(segment_file)):
+            print("Parsing...", segment_file)
+            with open(segment_file, 'r') as file:
                 for line in file:
                     key_value_data = parse_and_transform(line, column_names)
                     batch.append(key_value_data)
-                    pbar.update(1)
 
                     # Write to the same output file for each batch
                     if len(batch) == batch_size:
@@ -44,18 +62,22 @@ def process_file_in_batches(file_path, output_folder, column_names, table_name, 
                         counter += 1
                         batch = []
                         flag = True
-                        result = subprocess.run(sed_command, shell=True)
-                        result.check_returncode()
+                        # result = subprocess.run(sed_command, shell=True)
+                        # result.check_returncode()
 
             # Write the remaining data in the last batch
             if batch:
                 output_filename = os.path.join(output_folder, f'{table_name}{counter}.json')
+                print("Batch found at the end of file", segment_file)
                 write_to_file(batch, output_filename, mode='w')
-                result = subprocess.run(sed_command, shell=True)
-                result.check_returncode()
+                # result = subprocess.run(sed_command, shell=True)
+                # result.check_returncode()
+                os.remove(segment_file)
                 break
             if flag == False:
+                print("false flag")
                 break
+            os.remove(segment_file)
 
 def main():
     parser = argparse.ArgumentParser(description='Process .dat files and create JSON files.')
@@ -67,22 +89,20 @@ def main():
     script_directory = os.path.dirname(os.path.abspath(__file__))
     parent_directory = os.path.dirname(script_directory)
 
-    # Create a folder named 'json_data' in the script directory
-    output_folder = os.path.join(parent_directory, 'json_data')
-    os.makedirs(output_folder, exist_ok=True)
-
-    data_folder = os.path.join(parent_directory, 'data')
-    os.makedirs(data_folder, exist_ok=True)
-
     for table in tables_dict:
         table_name = table['table_name']
         batch_size = table['batch_size']
-        if (table_name == 'store'):
-            gen_data_command = f"cd ../tpcds-kit/tools && ./dsdgen -SCALE 10 -DIR ../../data -RNGSEED 1 -TABLE {table_name} && cd -"
+        if (table_name == 'inventory'):
+            gen_data_command = f"cd ../tpcds-kit/tools && ./dsdgen -SCALE 20 -DIR /media/user/fs2/data -RNGSEED 1 -TABLE {table_name} && cd -"
             process = subprocess.run(gen_data_command, shell=True)
             process.check_returncode()
 
-            file_path = os.path.join(data_folder, table_name + '.dat')
+            split_command = f"split -a4 -d -l {batch_size} {data_folder}/{table_name}.dat {data_folder}/{table_name}"
+            process = subprocess.run(split_command, shell=True)
+            process.check_returncode()
+            if (os.path.exists(file_path+'.dat') and not args.keep_source):
+                os.remove(file_path+'.dat')            
+            file_path = os.path.join(data_folder, table_name)
             column_names = table['column_names']
 
             # Process the file in batches and write to the same output file
@@ -94,6 +114,9 @@ def main():
                 batch_size = table['batch_size']
                 file_path = os.path.join(data_folder, table_name + '.dat')
                 column_names = table['column_names']
+                split_command = f"split -a4 -d -l {batch_size} {data_folder}/{table_name}.dat {data_folder}/{table_name}"
+                process = subprocess.run(split_command, shell=True)
+                process.check_returncode()
                 process_file_in_batches(file_path, output_folder, column_names, table_name, batch_size)
             elif table_name == 'web_sales':
                 table = tables_dict[22]
@@ -101,18 +124,24 @@ def main():
                 batch_size = table['batch_size']
                 file_path = os.path.join(data_folder, table_name + '.dat')
                 column_names = table['column_names']
+                split_command = f"split -a4 -d -l {batch_size} {data_folder}/{table_name}.dat {data_folder}/{table_name}"
+                process = subprocess.run(split_command, shell=True)
+                process.check_returncode()
                 process_file_in_batches(file_path, output_folder, column_names, table_name, batch_size)
             elif table_name == 'store_sales':
                 table = tables_dict[20]  # store returns
                 table_name = table['table_name']
                 batch_size = table['batch_size']
-                file_path = os.path.join(data_folder, table_name + '.dat')
+                file_path = os.path.join(data_folder, table_name)
                 column_names = table['column_names']
+                split_command = f"split -a4 -d -l {batch_size} {data_folder}/{table_name}.dat {data_folder}/{table_name}"
+                process = subprocess.run(split_command, shell=True)
+                process.check_returncode()
                 process_file_in_batches(file_path, output_folder, column_names, table_name, batch_size)
 
             # Remove the initial file only if the --keep-source argument is not provided
-            if (os.path.exists(file_path) and not args.keep_source):
-                os.remove(file_path)
+            if (os.path.exists(file_path+'.dat') and not args.keep_source):
+                os.remove(file_path+'.dat')
 
 if __name__ == "__main__":
     main()
